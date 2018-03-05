5ce36f3c522d96a56705417978cff34259a7b792
LEDDA Damien <damien.ledda@etu.unistra.fr>
2018-03-05 13:53:09 +0100

    Modification du jeu

--- jeu.c
+++ jeu.c
@@ -2,19 +2,21 @@
 
 int compte_voisins_vivants(int i, int j, grille g) {
   int v = 0;
-  int n=i-1, c=j-1;
+  int n, c;
   int templ, tempc;
-  if(n<0)
+  if(i==0)
     n=g.nbl-1;
-  if(c<0)
+  else n=i-1;
+  if(j==0)
     c=g.nbc-1;
-  for(templ=0; templ<3 && n<=i+1; templ++, n++){
-    for(tempc=0; tempc<3 && c<=j+1; tempc++, c++){
+  else c=j-1;
+  for(templ=0; templ<3; templ++, n++){
+    for(tempc=0; tempc<3; tempc++, c++){
       if(n>=g.nbl)
         n=0;
       if(c>=g.nbc)
         c=0;
-      if(g.cellules[n][c]!=0 && (n!=i && c!=j))
+      if(est_vivante(n, c, g))
         v++;
     }
   }
@@ -24,9 +26,13 @@ int compte_voisins_vivants(int i, int j, grille g) {
 void evolue(grille *g, grille *gc) {
   copie_grille (g,gc); // copie temporaire de la grille
   for (int i=0; i < g->nbl; i++) {
-    for (int j=0; j < g->nbc; ++j) {
-      /*set_vivante(i, j, *g);*/
-      /*set_morte(i, j, *g);*/
+    for (int j=0; j < g->nbc; j++) {
+      if(!est_vivante(i, j, *gc) && compte_voisins_vivants(i, j, *gc)==3)
+        set_vivante(i, j, *gc);
+      else if(est_vivante(i, j, *gc) &&
+       (compte_voisins_vivants(i, j, *gc)==3 || compte_voisins_vivants(i, j, *gc)==2))
+        set_vivante(i, j, *gc);
+      else set_morte(i, j, *gc);
     }
   }
 }
